e7ae1aa448ab497f5bd3a45194ad48412137344f
Redondo87 <amredondog@gmail.com>
2025-04-09 20:28:51 +0200

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  
+  {
+    path: '',
+    loadComponent: () =>
+      import('./app/empty/empty.component').then((m) => m.EmptyComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent)
+  }
 ];
-
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
