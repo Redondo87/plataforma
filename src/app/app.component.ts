a3164f837af15973d4defd35190d4844cc72ecfa
Redondo87 <amredondog@gmail.com>
2025-04-21 18:07:45 +0200

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'plataforma';
+  showLoginForm = false;
+  showRegisterForm = false;
+
+  toggleLoginForm() {
+    this.showLoginForm = !this.showLoginForm;
+    this.showRegisterForm = false; // Asegura que no se muestren ambos
+  }
+
+  showRegister() {
+    this.showRegisterForm = true;
+  }
+
+  showLogin() {
+    this.showRegisterForm = false;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() toggleLoginEvent = new EventEmitter<void>();
+
+  toggleLogin() {
+    this.toggleLoginEvent.emit();
+  }
+}
+
 
 
