219e084bd4489d66bd8022780369e7d5eaf32ff2
Redondo87 <amredondog@gmail.com>
2026-03-08 12:12:23 +0100

    Update libros.service.ts

--- Angular/src/app/services/libros.service.ts
+++ Angular/src/app/services/libros.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment'; 
 
 @Injectable({ providedIn: 'root' })
 export class LibrosService {
 
-  private API_URL = 'http://localhost:8080/api/libros-usuarios';
+  private API_URL = `${environment.apiBase}/api/libros-usuarios`;
+
   private GOOGLE_BOOKS_URL = 'https://www.googleapis.com/books/v1/volumes';
 
   constructor(private http: HttpClient) {}
 
-  // TOP libros (backend propio)
+  // TOP libros 
   obtenerTopLibros(): Observable<any[]> {
     return this.http.get<any[]>(`${this.API_URL}/top`);
   }
 
-  // DETALLE de libro (Google Books, usado en libro-detalle)
+  // DETALLE libro desde Google Books
   obtenerLibroPorId(id: string): Observable<any> {
     return this.http.get<any>(`${this.GOOGLE_BOOKS_URL}/${id}`);
   }
