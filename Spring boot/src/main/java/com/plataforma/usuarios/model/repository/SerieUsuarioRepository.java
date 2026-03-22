fae9d76268454d50c8e40cf2e7262e24b44d7797
Redondo87 <amredondog@gmail.com>
2026-03-22 20:03:05 +0100

    Update SerieUsuarioRepository.java

--- Spring boot/src/main/java/com/plataforma/usuarios/model/repository/SerieUsuarioRepository.java
+++ Spring boot/src/main/java/com/plataforma/usuarios/model/repository/SerieUsuarioRepository.java
@@ -1,7 +1,16 @@
 package com.plataforma.usuarios.model.repository;
+
 import com.plataforma.usuarios.model.SerieUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SerieUsuarioRepository extends JpaRepository<SerieUsuario, Long> {
-}
+import java.util.List;
+import java.util.Optional;
 
+public interface SerieUsuarioRepository extends JpaRepository<SerieUsuario, Long> {
+
+    // Listar todas las series/películas de un usuario
+    List<SerieUsuario> findByUsuario_Id(Long usuarioId);
+
+    // Buscar una serie/película concreta por usuario + itemId
+    Optional<SerieUsuario> findByUsuario_IdAndItemId(Long usuarioId, Long itemId);
+}
