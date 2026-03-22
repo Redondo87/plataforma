6b3a84479d94641d0c94afe7346a89fdb6871b9d
Redondo87 <amredondog@gmail.com>
2026-03-22 20:03:02 +0100

    Update LibroUsuarioRepository.java

--- Spring boot/src/main/java/com/plataforma/usuarios/model/repository/LibroUsuarioRepository.java
+++ Spring boot/src/main/java/com/plataforma/usuarios/model/repository/LibroUsuarioRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LibroUsuarioRepository extends JpaRepository<LibroUsuario, Long> {
 
-    List<LibroUsuario> findByUsuarioId(Long usuarioId);
+    List<LibroUsuario> findByUsuario_Id(Long usuarioId);
+
+    Optional<LibroUsuario> findByUsuario_IdAndLibroId(Long usuarioId, String libroId);
 
     @Query("""
         SELECT l.libroId, AVG(l.puntuacion), COUNT(l.puntuacion)
